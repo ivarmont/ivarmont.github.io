7b4eed2f8a4e381986be4adfd823b58c514c503a
mauricio.montellano <ivar.montellano@gmail.com>
2020-09-04 12:12:48 +0200

    add birthday page

---
+++ script.js
@@ -0,0 +1,48 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`November 08 ${currentYear} 00:00:00`);
+
+//Set background year
+year.innerHTML = currentYear;
+
+//Update countdown time
+function updateCountDown() {
+    const currentTime = new Date();
+
+    const diff = newYearTime - currentTime;
+    
+    if (diff < 0){
+        newYearTime = new Date(`November 08 ${currentYear+1} 00:00:00`);
+        const diff = newYearTime - currentTime;
+        year.innerHTML = currentYear + 1;
+    }
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    // Add values to DOM
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+
+}
+
+//Show spinner before countdown
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+}, 1000)
+
+//Run every second
+setInterval(updateCountDown, 1000);
